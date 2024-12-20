b2ae4c74bd9732b4862807623bb6db62ae42d98c
Soumik Hazra <soumikhazra100@gmail.com>
2024-12-21 04:08:23 +0530

    Update API base URL to point to the new endpoint for minigolf service

--- lib/api.dart
+++ lib/api.dart
@@ -1,4 +1,4 @@
 class Api {
   static const String baseUrl =
-      'https://script.google.com/macros/s/AKfycbwy-p8bwLNYWLzfs7UYDP24MTtQN9LWgPg3Gxiv_q3iIGFWfMoO0tja3M2BfoCDS7ASww/exec';
+      'https://app.forcempower.com/minigolf/api/v1/index.php';
 }
